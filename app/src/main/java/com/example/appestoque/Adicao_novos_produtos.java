16eef6504f0bf12c6f780b9cac119fd7117d8081
CarolineAn <anacarolinesn@outlook.com>
2023-01-27 14:14:54 -0300

    Telas restantes

---
+++ app/src/main/java/com/example/appestoque/Adicao_novos_produtos.java
@@ -0,0 +1,13 @@
+package com.example.appestoque;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adicao_novos_produtos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicao_novos_produtos);
+    }
+}
---
+++ app/src/main/java/com/example/appestoque/Tela_inicial_com_itens.java
@@ -0,0 +1,6 @@
+package com.example.appestoque;
+
+import android.app.Activity;
+
+public class Tela_inicial_com_itens extends Activity {
+}
