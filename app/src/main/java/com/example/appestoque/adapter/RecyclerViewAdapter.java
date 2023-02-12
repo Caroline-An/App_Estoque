c85d4b7e62a71e4318ea08348e3dbee3da499315
Giliarde_King <g1l14rd3@gmail.com>
2023-02-12 17:57:31 -0300

    Feat: ajustes nos metodos e torcendo para não quebrar

--- app/src/main/java/com/example/appestoque/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/appestoque/adapter/RecyclerViewAdapter.java
@@ -7,55 +7,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.appestoque.Cadastro;
 import com.example.appestoque.R;
 import com.example.appestoque.tela_itens;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
-
     Context context;
     String[] categorias;
-
     View viewOnCreate;
-
     ViewHolder viewHolderLocal;
-
     public RecyclerViewAdapter(Context contextRecebido, String[] categoriasRecebidas){
         context = contextRecebido;
         categorias = categoriasRecebidas;
     }
-
     public static class ViewHolder extends RecyclerView.ViewHolder{
-
         public TextView txtcategoria;
         public ImageView icone;
-
         public ViewHolder( View itemView) {
             super(itemView);
-
             txtcategoria = itemView.findViewById(R.id.txtCategoria);
             icone = itemView.findViewById(R.id.iconFoto);
-
         }
     }
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         viewOnCreate = LayoutInflater.from(context).inflate(R.layout.activity_recyclerview, parent, false);
         viewHolderLocal = new ViewHolder(viewOnCreate);
-
         return viewHolderLocal;
     }
-
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.txtcategoria.setText(categorias[position]);
-
         viewOnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,10 +49,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 v.getContext().startActivity(it);
             }
         });
-
     }
-
-    @Override
     public int getItemCount() {
         return categorias.length;
     }
