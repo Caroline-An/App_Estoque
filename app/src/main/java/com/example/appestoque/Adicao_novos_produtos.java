59ff549a5872d96d8d9f4974fd0010c0edc83da6
Giliarde_King <g1l14rd3@gmail.com>
2023-02-12 18:21:04 -0300

    Fix:  Crud produto v2

--- app/src/main/java/com/example/appestoque/Adicao_novos_produtos.java
+++ app/src/main/java/com/example/appestoque/Adicao_novos_produtos.java
@@ -24,7 +24,55 @@ import com.example.appestoque.dao.Produto;
 import com.example.appestoque.helper.DAO;
 
 
+public class Adicao_novos_produtos extends AppCompatActivity {
 
+    //king -- variáveis do produto
+
+        EditText nomep, descrp, qntp, categp, valorp;
+        //talvez não use o image view
+        ImageView camera;
+        Button addfoto, cadprod;
+        DAO banco;
+
+
+    //
+
+    //king -- barra inferior
+        ImageView home, novo, relatorio;
+        TextView homet, novot, relatoriot;
+    //
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicao_novos_produtos);
+
+
+
+        //king -- barra inferior
+            home = findViewById(R.id.iconHome);
+            homet = findViewById(R.id.idhome);
+            novo = findViewById(R.id.iconnovo);
+            novot = findViewById(R.id.idnovo);
+            relatorio = findViewById(R.id.iconrelatorio);
+            relatoriot = findViewById(R.id.idrelatorio);
+        //
+
+            banco = new DAO(this);
+
+        //king -- atributos do produto
+            nomep = findViewById(R.id.edit_nomeProd);
+            descrp = findViewById(R.id.edit_descricaoProd);
+            qntp = findViewById(R.id.edit_qntProd);
+            categp = findViewById(R.id.edit_categoriaProd);
+            valorp = findViewById(R.id.edit_valorProd);
+            camera = findViewById(R.id.iconFoto);
+            camera = findViewById(R.id.iconFoto);
+            addfoto = findViewById(R.id.botao_foto);
+            cadprod = findViewById(R.id.botao_cadastProd);
+        //
+
+        //king -- cadastrando produto
 
             cadprod.setOnClickListener(new View.OnClickListener() {
                 @Override
